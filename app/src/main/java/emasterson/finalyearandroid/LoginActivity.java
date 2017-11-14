75ab0a75a1ad81c2f06de4fa160b9a8e820f8322
EvanMasterson <evanm95@hotmail.com>
2017-11-14 12:56:58 +0000

    initial pages setup

--- app/src/main/java/emasterson/finalyearandroid/LoginActivity.java
+++ app/src/main/java/emasterson/finalyearandroid/LoginActivity.java
@@ -1,13 +1,49 @@
 package emasterson.finalyearandroid;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
 public class LoginActivity extends AppCompatActivity {
+
+    private Button loginBtn;
+    private TextView forgotPasswordTV;
+    private TextView signupTV;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+        loginBtn = findViewById(R.id.loginBtn);
+        forgotPasswordTV = findViewById(R.id.forgotPasswordTV);
+        signupTV = findViewById(R.id.signupTV);
+
+        loginBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent i = new Intent(getApplicationContext(), MainActivity.class);
+                startActivity(i);
+            }
+        });
+
+        forgotPasswordTV.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent i = new Intent(getApplicationContext(), PasswordResetActivity.class);
+                startActivity(i);
+            }
+        });
+
+        signupTV.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent i = new Intent(getApplicationContext(), SignUpActivity.class);
+                startActivity(i);
+            }
+        });
     }
 }
