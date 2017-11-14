1d31a1f90cbef5c45f3fa2e04b5e36f20f5def0f
EvanMasterson <evanm95@hotmail.com>
2017-11-14 20:38:12 +0000

    firebase login auth setup

--- app/src/main/java/emasterson/finalyearandroid/LoginActivity.java
+++ app/src/main/java/emasterson/finalyearandroid/LoginActivity.java
@@ -1,18 +1,35 @@
 package emasterson.finalyearandroid;
 
+import android.app.ProgressDialog;
 import android.content.Intent;
+import android.support.annotation.NonNull;
+import android.support.annotation.VisibleForTesting;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.text.TextUtils;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
 
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+/*
+    @Reference https://github.com/firebase/quickstart-android/tree/master/auth
+ */
 public class LoginActivity extends AppCompatActivity {
 
     private Button loginBtn;
     private TextView forgotPasswordTV;
     private TextView signupTV;
+    private EditText emailET;
+    private EditText passwordET;
 
+    private FirebaseAuth auth;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -21,12 +38,15 @@ public class LoginActivity extends AppCompatActivity {
         loginBtn = findViewById(R.id.loginBtn);
         forgotPasswordTV = findViewById(R.id.forgotPasswordTV);
         signupTV = findViewById(R.id.signupTV);
+        emailET = findViewById(R.id.emailET);
+        passwordET = findViewById(R.id.passwordET);
+
+        auth = FirebaseAuth.getInstance();
 
         loginBtn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Intent i = new Intent(getApplicationContext(), MainActivity.class);
-                startActivity(i);
+                signIn(emailET.getText().toString(), passwordET.getText().toString());
             }
         });
 
@@ -45,5 +65,83 @@ public class LoginActivity extends AppCompatActivity {
                 startActivity(i);
             }
         });
+    }
+
+    @Override
+    protected void onStart(){
+        super.onStart();
+        FirebaseUser currentUser = auth.getCurrentUser();
+        confirmUser(currentUser);
+    }
+
+    private void signIn(String email, String password) {
+        if (!validateForm()) {
+            return;
+        }
+        showProgressDialog();
+
+        auth.signInWithEmailAndPassword(email, password).addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        if (task.isSuccessful()) {
+                            FirebaseUser currentUser = auth.getCurrentUser();
+                            confirmUser(currentUser);
+                        } else {
+                            Toast.makeText(getApplicationContext(), "Authentication failed.",
+                                    Toast.LENGTH_SHORT).show();
+                            confirmUser(null);
+                        }
+                        hideProgressDialog();
+                    }
+                });
+    }
+
+    private boolean validateForm() {
+        boolean valid = true;
+
+        String email = emailET.getText().toString();
+        if (TextUtils.isEmpty(email)) {
+            emailET.setError("Required.");
+            valid = false;
+        } else {
+            emailET.setError(null);
+        }
+
+        String password = passwordET.getText().toString();
+        if (TextUtils.isEmpty(password)) {
+            passwordET.setError("Required.");
+            valid = false;
+        } else {
+            passwordET.setError(null);
+        }
+
+        return valid;
+    }
+
+    private void confirmUser(FirebaseUser user) {
+        hideProgressDialog();
+        if (user != null) {
+            Intent i = new Intent(getApplicationContext(), MainActivity.class);
+            startActivity(i);
+        }
+    }
+
+    @VisibleForTesting
+    public ProgressDialog mProgressDialog;
+
+    public void showProgressDialog() {
+        if (mProgressDialog == null) {
+            mProgressDialog = new ProgressDialog(this);
+            mProgressDialog.setMessage(getString(R.string.loading));
+            mProgressDialog.setIndeterminate(true);
+        }
+
+        mProgressDialog.show();
+    }
+
+    public void hideProgressDialog() {
+        if (mProgressDialog != null && mProgressDialog.isShowing()) {
+            mProgressDialog.dismiss();
+        }
     }
 }
