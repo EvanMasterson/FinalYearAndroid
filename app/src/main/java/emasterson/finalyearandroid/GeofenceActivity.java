58fb01da9ffd23f43db9d46411bbbb60d1bc221d
EvanMasterson <evanm95@hotmail.com>
2017-12-02 13:01:15 +0000

    maps added

--- app/src/main/java/emasterson/finalyearandroid/GeofenceActivity.java
+++ app/src/main/java/emasterson/finalyearandroid/GeofenceActivity.java
@@ -2,7 +2,14 @@ package emasterson.finalyearandroid;
 
 import android.os.Bundle;
 
-public class GeofenceActivity extends BaseActivity {
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.MapFragment;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.MarkerOptions;
+
+public class GeofenceActivity extends BaseActivity implements OnMapReadyCallback{
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -10,5 +17,14 @@ public class GeofenceActivity extends BaseActivity {
         setContentView(R.layout.activity_geofence);
 
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        MapFragment mapFragment = (MapFragment) getFragmentManager().findFragmentById(R.id.mapFragment);
+        mapFragment.getMapAsync(this);
+    }
+
+    @Override
+    public void onMapReady(GoogleMap googleMap) {
+        LatLng watch = new LatLng(-33.852, 151.211);
+        googleMap.addMarker(new MarkerOptions().position(watch).title("Current Watch Location"));
+        googleMap.moveCamera(CameraUpdateFactory.newLatLng(watch));
     }
 }
--- app/src/main/java/emasterson/finalyearandroid/MainActivity.java
+++ app/src/main/java/emasterson/finalyearandroid/MainActivity.java
@@ -1,12 +1,52 @@
 package emasterson.finalyearandroid;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
-public class MainActivity extends BaseActivity {
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.MapFragment;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.MarkerOptions;
+
+public class MainActivity extends BaseActivity implements OnMapReadyCallback{
+    private Button geofenceBtn, extraInfoBtn;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        geofenceBtn = findViewById(R.id.geofenceBtn);
+        extraInfoBtn = findViewById(R.id.extraInfoBtn);
+
+        geofenceBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent i = new Intent(getApplicationContext(), GeofenceActivity.class);
+                startActivity(i);
+            }
+        });
+
+        extraInfoBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent i = new Intent(getApplicationContext(), ExtraInfoActivity.class);
+                startActivity(i);
+            }
+        });
+
+        MapFragment mapFragment = (MapFragment) getFragmentManager().findFragmentById(R.id.mapFragment);
+        mapFragment.getMapAsync(this);
+    }
+
+    @Override
+    public void onMapReady(GoogleMap googleMap) {
+        LatLng watch = new LatLng(-33.852, 151.211);
+        googleMap.addMarker(new MarkerOptions().position(watch).title("Current Watch Location"));
+        googleMap.moveCamera(CameraUpdateFactory.newLatLng(watch));
     }
 }
