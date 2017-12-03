4745536fd9deded856ccc1442afdf0a99f5caced
EvanMasterson <evanm95@hotmail.com>
2017-12-03 16:01:58 +0000

    extra info gui, sample chart

--- app/src/main/java/emasterson/finalyearandroid/ExtraInfoActivity.java
+++ app/src/main/java/emasterson/finalyearandroid/ExtraInfoActivity.java
@@ -1,6 +1,17 @@
 package emasterson.finalyearandroid;
 
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.DatePicker;
+import android.widget.Spinner;
+
+import com.jjoe64.graphview.DefaultLabelFormatter;
+import com.jjoe64.graphview.GraphView;
+import com.jjoe64.graphview.series.DataPoint;
+import com.jjoe64.graphview.series.LineGraphSeries;
+
+import java.text.NumberFormat;
+import java.util.Calendar;
 
 public class ExtraInfoActivity extends BaseActivity {
 
@@ -10,5 +21,37 @@ public class ExtraInfoActivity extends BaseActivity {
         setContentView(R.layout.activity_extra_info);
 
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        Spinner spinner = findViewById(R.id.spinner);
+        ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(this, R.array.spinner, android.R.layout.simple_spinner_item);
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        spinner.setAdapter(adapter);
+
+        Calendar calendar = Calendar.getInstance();
+        int year = calendar.get(calendar.YEAR);
+        int month = calendar.get(calendar.MONTH);
+        int day = calendar.get(calendar.DAY_OF_MONTH);
+
+        DatePicker datePicker = findViewById(R.id.datePicker);
+        GraphView graph = findViewById(R.id.graph);
+        NumberFormat nf = NumberFormat.getInstance();
+//        nf.setMinimumFractionDigits(2);
+//        nf.setMinimumIntegerDigits(2);
+
+//        graph.getGridLabelRenderer().setLabelFormatter(new DefaultLabelFormatter(nf, nf));
+        LineGraphSeries<DataPoint> series = new LineGraphSeries<>(new DataPoint[] {
+                new DataPoint(0, 68),
+                new DataPoint(1, 78),
+                new DataPoint(2, 72),
+                new DataPoint(3, 84),
+                new DataPoint(4, 65)
+        });
+        graph.addSeries(series);
+
+        datePicker.init(year, month, day, new DatePicker.OnDateChangedListener() {
+            @Override
+            public void onDateChanged(DatePicker view, int year, int monthOfYear, int dayOfMonth) {
+
+            }
+        });
     }
 }
--- app/src/main/java/emasterson/finalyearandroid/PatientProfileActivity.java
+++ app/src/main/java/emasterson/finalyearandroid/PatientProfileActivity.java
@@ -3,11 +3,12 @@ package emasterson.finalyearandroid;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
-public class PatientProfileActivity extends AppCompatActivity {
+public class PatientProfileActivity extends BaseActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_patient_profile);
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
     }
 }
